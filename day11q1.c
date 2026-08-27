c19fc9776dfb40b7be231a1db1a2e85556c67108
madhavsharma-c <Madhav.32174@stu.upes.ac.in>
2026-08-27 10:51:12 +0530

    add day11q1

--- day11q1.c
+++ day11q1.c
@@ -13,3 +13,58 @@ Output 2:
 December, 31 days
 
 */
+
+#include<stdio.h>
+int main()
+{
+    int  n;
+    do{
+         printf(" enter the month number \n");
+         scanf("%d",&n);
+         switch(n)
+         {
+            case 1:
+            printf(" january, 31 days\n");
+            break;
+            case 2:
+            printf(" FEBRUARY , 28 days\n");
+            break;
+            case 3:
+            printf("MARCH, 31 days\n");
+            break;
+            case 4:
+            printf("APRIL, 30 days\n");
+            break;
+            case 5:
+            printf(" MARCH, 31 days\n");
+            break;
+            case 6:
+            printf(" JUNE, 30 days\n");
+            break;
+            case 7:
+            printf(" JULY, 31 days\n");
+            break;
+            case 8:
+            printf("AUGUST, 30 days\n");
+            break;
+            case 9:
+            printf(" SEPTEMBER , 31 days\n");
+            break;
+            case 10:
+            printf(" OCTOBER, 30 DAYS❤️\n");
+            break;
+            case 11:
+            printf(" NOVEMBER, 31 days\n");
+            break;
+            case 12:
+            printf("DECEMBER, 30 days\n");
+            break;
+            default:
+            printf(" invalid choice\n");
+
+         }
+
+    }
+    while( n!=12);
+    return 0;
+}
