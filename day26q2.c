14b77288f924b5344a00cc41c545edb5fda53245
madhavsharma-c <Madhav.32174@stu.upes.ac.in>
2026-09-15 22:35:16 +0530

    add

---
+++ day26q2.c
@@ -0,0 +1,39 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 1; i <= 13; i++) {
+        printf("* ");
+    }
+    return 0;
+}
