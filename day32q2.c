5df672abf3d38fcdf8aaa3ea42710e0ef4c54cf4
madhavsharma-c <Madhav.32174@stu.upes.ac.in>
2026-09-16 15:18:11 +0530

    commit

---
+++ day32q2.c
@@ -0,0 +1,39 @@
+//Q64: Find the digit that occurs the most times in an integer number.
+
+/*
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+
+*/
+#include <stdio.h>
+
+int main() {
+    long long n;
+    int freq[10] = {0};  
+    int digit, maxDigit, maxFreq = 0;
+
+    printf("Enter an integer number: ");
+    scanf("%lld", &n);
+    while (n != 0) {
+        digit = n % 10;
+        freq[digit]++;
+        n /= 10;
+    }
+    for (int i = 0; i < 10; i++) {
+        if (freq[i] > maxFreq) {
+            maxFreq = freq[i];
+            maxDigit = i;
+        }
+    }
+    printf("The digit that occurs the most times is: %d\n", maxDigit);
+
+    return 0;
+}
